638d772a80bdfb0b236ad84151163d66ac9b1432
Bowl-of-noodles <72003879+Bowl-of-noodles@users.noreply.github.com>
2021-02-13 11:38:07 +0300

    Add files via upload

---
+++ src/edu/rogachova/water/WaterLevel.java
@@ -0,0 +1,39 @@
+package edu.rogachova.water;
+
+import edu.rogachova.ability.ReasonToWorry;
+import edu.rogachova.description.Danger;
+import edu.rogachova.description.WaterType;
+import edu.rogachova.entities.Piglet;
+
+public class WaterLevel
+{
+    public WaterLevel(){
+
+    }
+
+    public boolean changeWaterLevel(Piglet piglet, Groove[] grooves){
+        System.out.println("\nУровень воды стал повышаться");
+        boolean beginingOfFlood = false;
+        Danger danger = new Danger();
+        danger.setDanger(ReasonToWorry.FLOOD);
+        for(Groove groove:grooves)
+        {
+            groove.startToFlow();
+        }
+        Stream stream = new Stream(grooves[0].getnWater());
+        stream.startToFlow();
+        River river = new River(stream.getnWater());
+        river.startToFlow();
+        if(river.getnWater()>=30){
+            System.out.println("Река вышла из своих берегов");
+            beginingOfFlood = true;
+        }
+        else{
+            piglet.getHome().setDanger(danger);
+            piglet.getHome().setWaterType(WaterType.RIVER);
+            piglet.startToWorry();
+            System.out.println("Но воды еще было недостаточно для наводнения");
+        }
+        return beginingOfFlood;
+    }
+}
