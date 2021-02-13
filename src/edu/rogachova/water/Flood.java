3199417cb210d6cef04d0a5981662bcf9d97f8c2
Bowl-of-noodles <72003879+Bowl-of-noodles@users.noreply.github.com>
2021-02-13 11:37:42 +0300

    Update Flood.java

--- src/edu/rogachova/water/Flood.java
+++ src/edu/rogachova/water/Flood.java
@@ -1,35 +1,21 @@
 package edu.rogachova.water;
 
-import edu.rogachova.ability.ReasonToWorry;
 import edu.rogachova.description.Danger;
 import edu.rogachova.description.WaterType;
 import edu.rogachova.entities.Piglet;
 
 public class Flood
 {
+    Danger danger;
     public Flood(){
 
     }
 
-    public void floodStart(Groove groove,Piglet piglet){
-        Danger danger = new Danger();
-        danger.setDanger(ReasonToWorry.FLOOD);
-        groove.startToFlow();
-        Stream stream = new Stream(groove.getnWater());
-        stream.startToFlow();
-        River river = new River(stream.getnWater());
-        river.startToFlow();
-        if(river.getnWater()>=30){
-            piglet.getHome().setDanger(danger);
-            piglet.getHome().setWaterType(WaterType.FLOOD);
-            System.out.println("Реки выщди за свои берега. Началось наводнение");
-        }
-        else{
-
-            piglet.getHome().setDanger(danger);
-            piglet.getHome().setWaterType(WaterType.RIVER);
-            System.out.println("Но воды еще было недостаточно для наводнения");
-        }
+    public void startFlood(Danger danger, Piglet piglet){
+        this.danger = danger;
+        System.out.println("Снаружи началось наводнение");
+        piglet.getHome().setDanger(danger);
+        piglet.getHome().setWaterType(WaterType.FLOOD);
         piglet.startToWorry();
     }
 }
