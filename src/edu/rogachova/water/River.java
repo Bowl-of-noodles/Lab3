5c2d4c13c9ed806e100bf8f7ae692caf6d3fbc49
Bowl-of-noodles <72003879+Bowl-of-noodles@users.noreply.github.com>
2021-02-22 13:33:03 +0300

    Update River.java

--- src/edu/rogachova/water/River.java
+++ src/edu/rogachova/water/River.java
@@ -37,4 +37,30 @@ public class River extends Hydrosphere
         nWater *= 5;
         System.out.println("Река наполнилась водой");
     }
+    
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof River)) return false;
+        if (!super.equals(o)) return false;
+        River river = (River) o;
+        return Double.compare(river.getnWater(), getnWater()) == 0 &&
+                getWaterType() == river.getWaterType();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(super.hashCode(), getWaterType(), getnWater());
+    }
+
+    @Override
+    public String toString()
+    {
+        return "River{" +
+                "waterType=" + waterType +
+                ", nWater=" + nWater +
+                '}';
+    }
 }
