59927837796884141837b836d85be833a2687162
Bowl-of-noodles <72003879+Bowl-of-noodles@users.noreply.github.com>
2021-03-06 14:51:30 +0300

    Update Hydrosphere.java

--- src/edu/rogachova/water/Hydrosphere.java
+++ src/edu/rogachova/water/Hydrosphere.java
@@ -22,7 +22,7 @@ public class Hydrosphere implements Flowable
         return waterType;
     }
 
-    public void setWaterType(WaterType waterType)
+    public final void setWaterType(WaterType waterType)
     {
         this.waterType = waterType;
     }
